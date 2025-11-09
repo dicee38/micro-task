b8122e3f59f07f36b42d42a724b086a38472518e
Dicee <demidov-is@mail.ru>
2025-11-09 17:46:49 +0300

    feat(logger): centralize logger for users and orders

--- service_users/shared/logger.js
+++ service_users/shared/logger.js
@@ -2,18 +2,8 @@ const { createLogger, format, transports } = require("winston");
 
 const logger = createLogger({
   level: "info",
-  format: format.combine(
-    format.timestamp(),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: { service: "microservice" },
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  ],
+  format: format.combine(format.timestamp(), format.json()),
+  transports: [new transports.Console()],
 });
 
 module.exports = logger;
